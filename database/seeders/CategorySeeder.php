f05298b1352f4638ab6116ce82c9fe8a84280e18
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2023-02-25 08:15:26 +0900

    カテゴリーデータ作成

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = \Faker\Factory::create();
+        DB::table('categories')->insert([
+            'name' => $faker->city()
+        ]);
+    }
+}
