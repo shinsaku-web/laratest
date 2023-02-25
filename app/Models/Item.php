89c4355d1f06949ddee2814a5ba34b26a0033dd5
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2023-02-25 09:08:15 +0900

    リレーション

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -9,4 +9,9 @@ class Item extends Model
 {
     use HasFactory;
     protected $fillable = ['name', "price"];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
