834347d40beff2938805acc5d095d14427b31d04
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2023-02-25 09:36:53 +0900

    itemコントローラー作成

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -13,54 +13,81 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        $items = Item::with("category")->get();
+        return response()->json($items);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+    // public function create()
+    // {
+    //     //
+    // }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreItemRequest $request)
     {
-        //
+        try {
+            Item::create([
+                "name" => $request->name,
+                "price" => $request->price,
+            ]);
+            return response()->json("OK");
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), 401);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Item $item)
-    {
-        //
-    }
+    // public function show(Item $item)
+    // {
+    //     //
+    // }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Item $item)
+    public function edit($id)
     {
-        //
+        try {
+            $item = Item::with("category")->firstOrFail($id);
+            return response()->json($item);
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), $th->getCode());
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateItemRequest $request, Item $item)
+    public function update(UpdateItemRequest $request, $id)
     {
-        //
+        try {
+            Item::findOrFail($id)->update([
+                "name" => $request->name,
+                "price" => $request->price,
+            ]);
+            return response()->json("OK");
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), 401);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Item $item)
+    public function destroy($id)
     {
-        //
+        try {
+            Item::findOrFail($id)->delete();
+            return response()->json("OK");
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), 401);
+        }
     }
 }
